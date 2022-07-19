88417c982346da80786c2f2df83834e03968a903
ahmed rashad <104315358+a7med-rashad@users.noreply.github.com>
2022-07-19 18:42:18 +0200

    Update and rename index.Cart.js to index.js

--- src/redux/reducer/index.Cart.js
+++
@@ -1,1 +0,0 @@
-import handleCart form "./handleCart"
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,9 @@
+import handleCart form "./handleCart";
+import {combineReducers} from "redux";
+
+
+const rootReducers = combineReducers ({
+  handleCart,
+})
+
+export default rootReducers;
