43a3190d273b0465bd90ff78f09dc69fe86c189f
a7med-rashad <a7medra4aad3@gmail.com>
2022-08-02 13:41:56 +0200

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <div className=" about fs-1 text-center fw-bold p-2 m-5">About Us</div>
-      <div className="fs-5 text-center text-gray mx-5 ">You can buy any porduct from our website so we have a big collection form deffirent items </div>
+      <div className="fs-5 text-center text-gray mx-5 ">You can buy any product from our website so we have a big collection form different items </div>
       <div className="card text-center m-auto mt-5">
       <img src="images/big-sale.png" alt="" />
       </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,15 @@ const Contact = () => {
   return (
     <div>
       <div className="card text-center m-auto mt-5">
-      <img src="images/big-sale.png" alt="" />
+        <img src="images/big-sale.png" alt="" />
       </div>
-      <div className="fs-1 p-2 m-5">phone Namber : 01095353364</div>
-      <div className="fs-1 p-2 m-5">E-mail : a7medra4aad3@gmail.com</div>
+      <div className="cart fs-1 p-2 my-5 bg-dark text-center m-auto w-50 text-light">
+        <h1>phone number</h1>
+        <h2>01095353364</h2>
+      </div>
+      <div className="cart fs-1 p-2 my-5 bg-dark text-center m-auto w-50 text-light">
+        <h1>E-mail</h1> <a href="a7medra4aad3@gmail.com" className="text-decoration-none">a7medra4aad3@gmail.com</a>
+      </div>
     </div>
   );
 };
