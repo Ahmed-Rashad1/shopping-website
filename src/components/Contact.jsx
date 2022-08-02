e67b640f8be695e5107a2d93fa69827d3e5737b3
a7med-rashad <a7medra4aad3@gmail.com>
2022-08-02 15:09:57 +0200

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
+    <>
+          <div className=" about fs-1 text-center fw-bold p-2 m-5">Contact Us</div>
+      <div className="cart fs-1 p-2 my-5 bg-dark text-center m-auto w-50 text-light">
+        <h2>phone number</h2>
+        <h3>01095353364</h3>
+      </div>
+      <div className="cart fs-1 p-2 my-5 bg-dark text-center m-auto w-50 text-light">
+        <h2>E-mail</h2> <a href="a7medra4aad3@gmail.com" className="text-decoration-none fs-3">a7medra4aad3@gmail.com</a>
+      </div>
       <div className="card text-center m-auto mt-5">
         <img src="images/big-sale.png" alt="" />
       </div>
-      <div className="cart fs-1 p-2 my-5 bg-dark text-center m-auto w-50 text-light">
-        <h1>phone number</h1>
-        <h2>01095353364</h2>
-      </div>
-      <div className="cart fs-1 p-2 my-5 bg-dark text-center m-auto w-50 text-light">
-        <h1>E-mail</h1> <a href="a7medra4aad3@gmail.com" className="text-decoration-none">a7medra4aad3@gmail.com</a>
-      </div>
-    </div>
+    </>
   );
 };
 
